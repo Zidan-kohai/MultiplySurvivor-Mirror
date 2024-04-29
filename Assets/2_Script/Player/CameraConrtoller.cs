67bb4bf48468b1dded07ae902797ba0ebb59de85
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-04-29 11:25:35 +0300

    add first person camera

---
+++ Assets/2_Script/Player/CameraConrtoller.cs
@@ -0,0 +1,78 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraConrtoller : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private CinemachineVirtualCamera firstPersonCamera;
+    [SerializeField] private CinemachineVirtualCamera thirdPersonCamera;
+
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float minVerticalRotateClamp = -90f;
+    [SerializeField] private float maxVerticalRotateClamp = 90f;
+    [SerializeField] private float verticalRotation = 0f;
+    [SerializeField] private CameraState state;
+
+    //Need to Refactoring
+    private void Start()
+    {
+        switch (state)
+        {
+            case CameraState.First:
+                firstPersonCamera.gameObject.SetActive(true);
+                thirdPersonCamera.gameObject.SetActive(false);
+                break;
+            case CameraState.Third:
+                firstPersonCamera.gameObject.SetActive(false);
+                thirdPersonCamera.gameObject.SetActive(true);
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case CameraState.First:
+                FirstPersonConrtoller();
+                break;
+            case CameraState.Third:
+                ThirdPersonConrtoller();
+                break;
+        }
+    }
+
+    public void ChangeState(CameraState newState)
+    {
+        if (newState == state) return;
+
+        state = newState;
+
+        switch (state)
+        {
+            case CameraState.First:
+                firstPersonCamera.gameObject.SetActive(true);
+                thirdPersonCamera.gameObject.SetActive(false);
+                break;
+            case CameraState.Third:
+                firstPersonCamera.gameObject.SetActive(false);
+                thirdPersonCamera.gameObject.SetActive(true);
+                break;
+        }
+    }
+
+    private void FirstPersonConrtoller()
+    {
+        float mouseY = inputManager.GetMouseDeltaY * mouseSensitivity * Time.deltaTime;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, minVerticalRotateClamp, maxVerticalRotateClamp);
+        firstPersonCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+
+    private void ThirdPersonConrtoller()
+    {
+        float mouseY = inputManager.GetMouseDeltaY * mouseSensitivity * Time.deltaTime;
+    }
+}
---
+++ Assets/2_Script/Player/CameraState.cs
@@ -0,0 +1,5 @@
+﻿public enum CameraState
+{
+    First,
+    Third
+}
